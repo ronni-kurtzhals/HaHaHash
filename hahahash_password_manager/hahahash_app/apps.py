b55cd6236c5c5cff30b09eecf310d9a8a8d0c347
Ronni Kurtzhals <rckurtzhals@gmail.com>
2023-11-19 14:39:35 -0600

    Added basic Django framework for API.

---
+++ hahahash_password_manager/hahahash_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HahahashAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'hahahash_app'
---
+++ hahahash_password_manager/hahahash_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class PasswordEntry(models.Model):
+    website = models.CharField(max_length=255)
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.website
